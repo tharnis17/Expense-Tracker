12f3c2af12aeb193cc78eca31079a203c9b3e2ba
cleverprogramer <166504205+tharnis17@users.noreply.github.com>
2026-06-25 14:14:04 +0530

    Update server.js

--- expense-tracker-backend-main/server.js
+++ expense-tracker-backend-main/server.js
@@ -14,7 +14,8 @@ connectDB();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://expense-tracker-eight-vert-64.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],

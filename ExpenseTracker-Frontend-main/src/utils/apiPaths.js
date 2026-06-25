26e151832f0c2aee7c2066514ed700fe835ad2cf
cleverprogramer <166504205+tharnis17@users.noreply.github.com>
2026-06-25 14:15:26 +0530

    Remove trailing slash from BASE_URL

--- ExpenseTracker-Frontend-main/src/utils/apiPaths.js
+++ ExpenseTracker-Frontend-main/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://expense-tracker-k96o.onrender.com/";
+export const BASE_URL = "https://expense-tracker-k96o.onrender.com";
 //export const BASE_URL = "http://localhost:8000";
 
 export const API_PATHS = {
